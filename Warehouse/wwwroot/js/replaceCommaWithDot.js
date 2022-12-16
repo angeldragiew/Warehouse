8351d23053ec00ad3fe2beb131d4271f2982b480
Angel Dragiev <angeldragieww@gmail.com>
2022-12-16 19:12:19 +0200

    Added js to replace comma with dot when loading edit page.

---
+++ Warehouse/wwwroot/js/replaceCommaWithDot.js
@@ -0,0 +1,10 @@
+﻿function replace() {
+    var priceInputs = document.getElementsByClassName('prices');
+
+    Array.from(priceInputs).forEach(priceInput => {
+        priceInput.value = priceInput.value.replace(',', '.')
+    })
+}
+
+replace();
+
