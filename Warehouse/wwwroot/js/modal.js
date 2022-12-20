3620f903c0c495adf10ef9c4a626def5bacd53a7
Angel Dragiev <angeldragieww@gmail.com>
2022-12-20 14:59:45 +0200

    Added modal for product images

---
+++ Warehouse/wwwroot/js/modal.js
@@ -0,0 +1,21 @@
+﻿function enableModal() {
+    var modal = document.getElementById('myModal');
+
+    var images = document.getElementsByClassName('product-img');
+
+    Array.from(images).forEach(img => {
+        img.addEventListener('click', function (e) {
+            document.getElementById('modal-img').src = e.target.src;
+            modal.style.display = 'block';
+
+        })
+    })
+
+    window.addEventListener('click', function (e) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    })
+}
+
+enableModal();
